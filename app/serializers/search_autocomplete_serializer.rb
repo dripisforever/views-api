1e2e9f7ce46479fd7859f3227c273adf400e8445
strivemag <talga.tawit@gmail.com>
2017-04-06 15:39:14 +0600

    edit serializer

--- app/serializers/search_autocomplete_serializer.rb
+++ app/serializers/search_autocomplete_serializer.rb
@@ -10,11 +10,3 @@ class SearchSerializer < ActiveModel::Serializer
   end
 
 end
-json.users do |json|
-  json.array! @users do |user|
-    json.id user.id
-    json.username user.username
-    json.avatar_url user.avatar_url.present? ? user.avatar_url : image_path('default-avatar.svg')
-    json.url user_path(user.slug)
-  end
-end
